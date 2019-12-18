335399a147c919b9f1c0ec41a67d5f80381f7872
Thomas <thomas.cacheux@etu.univ-valenciennes.fr>
2019-12-18 11:07:42 +0100

    fix style batiment

--- components/listBatiments.js
+++ components/listBatiments.js
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 18,
         height: 44,
-        //textDecorationLine: 'underline'
+        textDecorationLine: 'underline'
     },
     row: {
         flexDirection: 'row',
