3c5b2f0b41c8a4847f5a50b85a3e1ce4de19125c
Jimmy Trackoen <j.trackoen9@gmail.com>
2020-01-04 11:20:13 +0100

    Changement de l'adresse IP de l'API pour la faire matché a l'adresse IP Elastique

--- datas/ip-info.js
+++ datas/ip-info.js
@@ -1,2 +1,2 @@
-export const IP = 'http://3.94.184.209/';
-export const IP_NOTIFICATION = 'http://3.94.184.209:3000/'
+export const IP = 'http://54.175.139.69/';
+export const IP_NOTIFICATION = 'http://54.175.139.69:3000/'
